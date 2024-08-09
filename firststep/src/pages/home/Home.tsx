4868f3d12dcad4854ec38a17f7685fab5648adfe
Luis Gustavo Sinastre <luisgustavosinastre13@gmail.com>
2024-08-09 18:03:28 -0300

    Components and buttons

--- firststep/src/pages/home/Home.tsx
+++ firststep/src/pages/home/Home.tsx
@@ -1,6 +1,15 @@
 export const Home = () => {
 
     return(
+        <div>
         <p>Home</p>
+        <Button />
+        </div>
     )
+}
+
+const Button = () => {
+
+    return <button>Search</button>
+
 }
