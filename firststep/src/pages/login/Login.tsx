6fa0744829e0c7ed6be9eb3ff208208105aa6184
Luis Gustavo Sinastre <luisgustavosinastre13@gmail.com>
2024-08-14 17:40:01 -0300

    feat: add a form to login

--- firststep/src/pages/login/Login.tsx
+++ firststep/src/pages/login/Login.tsx
@@ -1,19 +1,37 @@
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
 
 export const Login = () => {
 
+    const [email, setEmail] = useState('');
+    const [senha, setSenha] = useState('');
+    
     const navigate = useNavigate();
 
-    const HandleClick = () => {
-        navigate('/home');  // Chamando navigate diretamente
-    };
+    const handleSignIn = () => {
+        console.log(email)
+        console.log(senha)
+        navigate('/Home');  // Chamando navigate diretamente
+    }
 
     return (
         <div>
-            Login
-            <button onClick={HandleClick}>Home</button>
+            <form>
+                
+                <label>
+                    <span>E-mail</span>
+                    <input value={email} onChange={e => setEmail(e.target.value)}/>
+                </label>
+
+                <label>
+                    <span>Senha</span>
+                    <input type="password" value={senha} onChange={e => setSenha(e.target.value)}/>
+                </label>
+                
+                <button onClick={handleSignIn}>Login</button>
+
+            </form>
         </div>
     );
 };
-
 
