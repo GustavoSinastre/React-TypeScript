feab87502c7785713584567f90356ebbc8d0044c
Luis Gustavo Sinastre <luisgustavosinastre13@gmail.com>
2024-08-25 15:54:34 -0300

    Adding role to pages that will be used only with admin users

--- HelpDesk-Solution/frontend/src/components/LogoutButton.tsx
+++ HelpDesk-Solution/frontend/src/components/LogoutButton.tsx
@@ -1,19 +1,37 @@
 // src/components/LogoutButton.tsx
-
 import React from 'react';
 import { useAuth } from '../context/AuthContext';
-import { useNavigate } from 'react-router-dom'; // Importa o hook useNavigate
+import { useNavigate } from 'react-router-dom';
+import { Button } from '@mui/material';
+import { ExitToApp } from '@mui/icons-material';
 
 const LogoutButton: React.FC = () => {
     const { logout } = useAuth();
-    const navigate = useNavigate(); // Inicializa o hook useNavigate
+    const navigate = useNavigate();
 
     const handleLogout = () => {
-        logout(); // Chama a função de logout
-        navigate('/signin'); // Redireciona para a página de login
+        logout();
+        navigate('/signin');
     };
 
-    return <button onClick={handleLogout}>Logout</button>; // Renderiza o botão
+    return (
+        <Button
+            variant="contained"
+            color="secondary"
+            startIcon={<ExitToApp />}
+            onClick={handleLogout}
+            style={{
+                padding: '10px 20px',
+                borderRadius: '15px',
+                fontSize: '16px',
+                fontWeight: 'bold',
+                textTransform: 'none'
+            }}
+        >
+            Logout
+        </Button>
+    );
 };
 
 export default LogoutButton;
+
--- HelpDesk-Solution/frontend/src/pages/home/Home.tsx
+++ HelpDesk-Solution/frontend/src/pages/home/Home.tsx
@@ -1,20 +1,19 @@
 // src/pages/home/Home.tsx
 import React from 'react';
 import { useNavigate } from 'react-router-dom';
-import { Card, CardContent, Typography, Grid } from '@mui/material';
+import { Grid, Typography } from '@mui/material';
 import { Person, Assignment, CheckCircle, Build } from '@mui/icons-material';
-import LogoutButton from '../../components/LogoutButton'; // Importe o LogoutButton
+import LogoutButton from '../../components/LogoutButton';
+import { useAuth } from '../../context/AuthContext';
+import MenuCard from './components/MenuCard';
 
 const Home: React.FC = () => {
   const navigate = useNavigate();
-
-  const handleNavigation = (path: string) => {
-    navigate(path);
-  };
+  const { user } = useAuth();
 
   const menuItems = [
-    { title: 'Cadastro', icon: <Person style={{ fontSize: 40 }} />, path: '/cadastro' },
-    { title: 'Abertura de Chamados', icon: <Assignment style={{ fontSize: 40 }} />, path: '/ticket' },
+    ...(user?.role === 'admin' ? [{ title: 'Cadastro', icon: <Person style={{ fontSize: 40 }} />, path: '/cadastro' }] : []),
+    { title: 'Chamados', icon: <Assignment style={{ fontSize: 40 }} />, path: '/ticket' },
     { title: 'Aprovações', icon: <CheckCircle style={{ fontSize: 40 }} />, path: '/aprovacoes' },
     { title: 'Tratamento', icon: <Build style={{ fontSize: 40 }} />, path: '/tratamento' },
   ];
@@ -25,24 +24,16 @@ const Home: React.FC = () => {
         <Typography variant="h4" align="center" gutterBottom style={{ flexGrow: 1 }}>
           Bem-vindo ao Sistema HelpDesk
         </Typography>
-        <LogoutButton /> {/* Inclua o botão de logout aqui */}
+        <LogoutButton />
       </div>
       <Grid container spacing={4} justifyContent="center">
         {menuItems.map((item, index) => (
           <Grid item key={index} xs={12} sm={6} md={4}>
-            <Card
-              onClick={() => handleNavigation(item.path)}
-              style={{ cursor: 'pointer', textAlign: 'center', padding: '20px', transition: 'transform 0.3s', borderRadius: '15px' }}
-              onMouseOver={(e) => (e.currentTarget.style.transform = 'scale(1.05)')}
-              onMouseOut={(e) => (e.currentTarget.style.transform = 'scale(1)')}
-            >
-              <CardContent>
-                {item.icon}
-                <Typography variant="h5" component="div">
-                  {item.title}
-                </Typography>
-              </CardContent>
-            </Card>
+            <MenuCard
+              title={item.title}
+              icon={item.icon}
+              onClick={() => navigate(item.path)}
+            />
           </Grid>
         ))}
       </Grid>
